87123124dfc8766a59050d48fd9ee7e328f7da22
haged18 <haged3500@gmail.com>
2026-02-22 00:14:41 +0200

    made some updates AND fixed some things
    1]1fixed the navbar
    2-filled the white gapes that surrounded the navbar
    3-fixed the mobile view on all pages
    4-updated the contact page
    5-fixed the makeyourown banner
    6-updated the html.index
    7-updated the navbar contents font

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,36 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/logo.png'; 
-import '../styles/Navbar.css'; 
+import logo from '../assets/logo.png';
+import '../styles/Navbar.css';
 
 function AppNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="custom-navbar">
-<Container>
-  <Navbar.Brand href="/">          {/* logo */}
-    <img src={logo} className="logo" alt="Glow Aroma" />
-  </Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" variant="light" className="custom-navbar">
+      <Container>
+        {/* Brand / Logo */}
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Glow Aroma Logo" className="logo" />
+        </Navbar.Brand>
 
+        {/* The Hamburger Menu Toggle Button */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="links">   {/* ms-auto pushes to right */}
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
-      <Nav.Link href="/products">Products</Nav.Link>
-      <Nav.Link href="/cart" className='cart'></Nav.Link>
-      <Nav.Link href="/profile" className='profile'></Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Container>
+        {/* Collapsible content */}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto align-items-center">
+            <Nav.Link className="home" href="/">Home</Nav.Link>
+            <Nav.Link className="contact" href="/contact">Contact</Nav.Link>
+            <Nav.Link className="products" href="/products">Products</Nav.Link>
+            
+            {/* The wrapper that keeps icons side-by-side */}
+            <div className="icon-group">
+              <Nav.Link href="/cart" className="cart"></Nav.Link>
+              <Nav.Link href="/profile" className="profile"></Nav.Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import '../styles/contact.css'
+import '../styles/contact.css';
 
 const Contact = () => {
   return (
     <div className="home-container">
       <Navbar />
-      <h1>Contact Us</h1>
-      <hr className='hr--small'></hr>
-      <div className='inputs'>
-        <div className='top'>
-          <input type="text" className='name' placeholder='Name'></input>
-          <input typr='number' className='number' placeholder='Number'></input>
-        </div>
-        <div className='middle'>
-          <input type='email' className='contact-email' placeholder='email@example.com'></input>
-        </div>
-        <div className='bottom'>
-          <input type="text" className='message' placeholder='Message'></input>
-        </div>
-          <button className='send'></button>
+      
+      <div className="contact-wrapper">
+        <h1>Contact Us</h1>
+        <hr className="hr--small" />
+        
+        {/* Swapped to a <form> tag for better web standards */}
+        <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
+          
+          {/* Top Row: Name and Number */}
+          <div className="input-row">
+            <input type="text" className="form-input" placeholder="Name" />
+            <input type="tel" className="form-input" placeholder="Phone Number" />
+          </div>
+          
+          {/* Middle Row: Email */}
+          <input type="email" className="form-input" placeholder="email@example.com" />
+          
+          {/* Bottom Row: Message (Changed to textarea!) */}
+          <textarea className="form-textarea" placeholder="Message"></textarea>
+          
+          {/* Added text to the button so it isn't invisible */}
+          <button type="submit" className="send-btn">Send Message</button>
+          
+        </form>
       </div>
-      </div>
+    </div>
   );
 };
 
