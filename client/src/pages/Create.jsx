9d0ba488097d1b889deff7ce90bc74f18fa8d2fb
Bellgello <bellogello121@gmail.com>
2026-02-22 01:12:59 +0200

    signup, create

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import candle from "../assets/candle.png"
+import "../styles/create.css"
 
 const Create = () => {
   return (
@@ -7,7 +9,34 @@ const Create = () => {
       <Navbar />
       <h1>Create Your Own Page</h1>
       <p>Welcome to Glow Aroma - Premium Candles</p>
-      <select className='scents' name='Scent'></select>
+      <div className='creation'>
+        
+        <div className='candle-div'>
+        <img className="candle-preview" src={candle}></img>
+      </div>
+      <div className='choices'> 
+
+        <div className='selects'>
+        <select className='cup'>
+          <option value="default">Choose Your Cup</option>
+        </select>
+
+        <select className='scents'>
+          <option value="default">Select a Scent</option>
+        </select>
+
+
+
+        <select className='color'>
+          <option value="default">Choose The Color</option>
+        </select>
+                </div>
+
+        <button className='confirm'>Confirm Candle</button>
+  
+
+</div>
+    </div>
     </div>
   );
 };
