3b06076391a790cb49703b76cd0b5e64d0d36e63
Belal Mahmoud <bellogello121@gmail.com>
2026-04-14 21:31:02 +0200

    should work now

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // This is built into Node.js
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // This line tells Vite: "No matter who asks for React, 
+      // always give them the one in the main node_modules folder."
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
 })
