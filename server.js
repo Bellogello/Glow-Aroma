8806c5a5bbbd857c3520bebce96421b5f6368bd6
Belal Mahmoud <bellogello121@gmail.com>
2026-04-15 01:29:39 +0200

    backend again 10

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 // --- MIDDLEWARE CONFIGURATION ---
 // ==========================================
 app.use(cors({
-    origin: '*',
+    origin: true,
     credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
