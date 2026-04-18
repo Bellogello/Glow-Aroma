826b33ad6e093a688ee6418647ab8a1d371728c5
Bellgello <bellogello121@gmail.com>
2026-04-18 11:28:08 +0200

    trying to make it go live using vercel

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ const app = express();
 // ==========================================
 // --- MIDDLEWARE CONFIGURATION ---
 // ==========================================
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+    origin: [
+        'https://glow-aroma.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
