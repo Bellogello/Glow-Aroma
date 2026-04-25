f14de8bee5314749be9350ebda0df71fa653c20a
Bellgello <bellogello121@gmail.com>
2026-04-25 11:02:39 +0300

    cloudinary issues 4

--- server.js
+++ server.js
@@ -853,8 +853,7 @@ app.delete('/admin/inventory/mold-shapes/:id', async (req, res) => {
 // ==========================================
 // --- SERVER ACTIVATION ---
 // ==========================================
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on port ${PORT}`);
 });
 
